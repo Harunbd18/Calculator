4c03818142b0c824ea0af883fbec0fc20aacce04
Al Zobaer <al.zobaer@yahoo.com>
2019-04-03 10:33:01 +0600

    Initial Commit with Calculator.java file

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -6,5 +6,12 @@ public class Calculator {
     public static void main(String[] args) {
         
     }
+	
+	double DivisionMethod ( double FirstValue, double SecondValue ) {
+		
+		double result =  FirstValue / SecondValue;
+		
+		return result;
+	}
     
 }
